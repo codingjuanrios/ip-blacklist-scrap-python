251d3db1cf9a2eb1b8b75a678002e810b3821eed
Juan Ríos <escribeajuanrios@gmail.com>
2020-04-23 23:07:48 +0200

    Conseguido el scrapeo de las cinco IP

--- ip_blacklist.py
+++ ip_blacklist.py
@@ -4,63 +4,63 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from time import sleep
+
+lista_servers = ["185.34.192.190", "134.0.9.43", "185.34.194.93", "134.0.8.167", "185.34.194.74"]
+srv01, srv02, srv03, srv04, srv05 = lista_servers
+
 #------------------------------ from selenium.webdriver.support.ui import Select
 
-#----------------------------------------------- PARA HACER VISIBLE EL NAVEGADOR
-browser = webdriver.Firefox()
-
 #---------------------------------------------- PARA HACER HEADLESS EL NAVEGADOR
-#------------------------ from selenium.webdriver.firefox.options import Options
-#----------------------------------------------------------- options = Options()
-#------------------------------------------------------- options.headless = True
-#---------------------------------- browser = webdriver.Firefox(options=options)
+from selenium.webdriver.firefox.options import Options
+options = Options()
+options.headless = True
 
-browser.get("https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a185.34.192.190&run=toolpage")
+
+#-------------------------------------------------------- MXTOOLBOX UTILIZA ASPX DESCARTADO
+# browser.get("https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a185.34.192.190&run=toolpage")
+
 
 #----------------------------------------------- POR SI HAY UN ELEMENTO DROPDOWN
 #------------------- dropdown = Select(browser.find_element_by_id('btnAction3'))
 #------------------------------------- dropdown.select_by_visible_text('Banana')
 
-# search_form = browser.find_element_by_id('ctl00_ContentPlaceHolder1_ucToolhandler_txtToolInput')
-#--------------------------------------- search_form.send_keys('185.34.192.190')
-# boton = browser.find_element_by_id('ctl00_ContentPlaceHolder1_ucToolhandler_btnAction')
-#----------------------------------------------------------------- boton.click()
-
-sleep(10)
-
-
-#--------------------------- results = browser.find_element_by_tag_name('table')
-
-page = BeautifulSoup(browser.page_source,"html.parser")
-
-links = page('table')
-
-#--------------------------------------------------------------- browser.close()
-
 file = open("./codigo_html", "w", encoding="utf-8")
-file.write(str(links))
+
+#PARA COMPROBAR CADA ELEMENTO DE LA LISTA
+for srv in lista_servers:
+    #PARA HACER VISIBLE EL NAVEGADOR
+    #--------------------------------------------- browser = webdriver.Firefox()
+    #PARA HACER INVISIBLE EL NAVEGADOR
+    browser = webdriver.Firefox(options=options)
+    
+    browser.get("https://www.blacklistmaster.com/check")
+    search_form = browser.find_element_by_name('ip')
+    search_form.send_keys(srv)
+    boton = browser.find_element_by_name('Submit')
+    boton.click()
+    
+    # HACER LA ESPERA DE 15 SEGUNDOS FUNCIONA PARA ESTA WEB https://www.blacklistmaster.com/check
+    sleep(15)
+    
+    page = BeautifulSoup(browser.page_source,"html.parser")
+    browser.close()
+    
+    #------------------------------ AQUI LOCALIZAMOS LA TABLA COMPLETA DE RESULTADOS
+    
+    links = page.select('.myip800 a')
+    
+    for link in links:
+        file.write("<p>")
+        file.write(str(srv))
+        file.write(": ")
+        file.write(str(link))
+        file.write("</p>")
+        if None in (link):
+            continue
+    print(f"{srv} terminado")
+
 file.close
 
-
-#---------------------------------------------------------- for link in results:
-#------------------------------------------------------------------------------ 
-    #----------------------------------------------- print(link.encode("utf-8"))
-
-#---------------------------------------- from selenium.webdriver import Firefox
-#------------------------ from selenium.webdriver.firefox.options import Options
-#------------------------------------------------------------ print('iniciando')
-#-------------------------------------------------------------- opts = Options()
-#---------------------------------------------------------- opts.headless = True
-#----------------------------------------------- browser = Firefox(options=opts)
-# browser.get('https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a185.34.192.190&run=toolpage')
-#----------- resultados = browser.find_elements_by_class_name('result-argument')
-#------------------------------------------------------------------------------ 
-#--------------------------------------------- file = open("./codigo_html", "w")
-#--------------------------------------------------- file.write(str(resultados))
-#-------------------------------------------------------------------- file.close
-#------------------------------------------------------------ print('avanzando')
-
-browser.close()
 print('terminado ')
 
 quit()
