9df674d12e13e1838abc2c46f687c7118441781c
Juan Ríos <escribeajuanrios@gmail.com>
2020-05-10 19:59:00 +0200

    Funcional con preguntas en al linea de comandos

--- ip_blacklist.py
+++ ip_blacklist.py
@@ -6,94 +6,176 @@ from selenium import webdriver
 from time import sleep
 import datetime, json
 
-
-with open("./listado.json", "r") as read_file:
-    dct_servers = json.load(read_file)
-
-#------------------------------------------------------------ #INCLUIR NUEVAS IP
-#-------------- incluir_ip = input("Hola! Quieres incluir alguna IP? (Si/No): ")
-#------------------------------------------------------------------------------ 
-#-------------------------------------------------------- if incluir_ip == "Si":
-    #----------------------------------- nombre = input("Nombre del servidor: ")
-    #-------------------------------------------------------- ip = input("IP: ")
-    #-------------------------------------------------- dct_servers[nombre] = ip
-    #--------------------------------------------------------- print("Incluido")
-#------------------------------------------------------------------------- else:
-    #----------------------------------------- print('No se incluye ninguna IP')
-
-
-#FECHA
-today = datetime.date.today()
-
-print("Iniciando scrapeo")
-
-#-------------------------------------- #LISTA DE LOS CINCO SERVIDORES DE MORGAN
-# lista_servers = ["185.34.192.190", "134.0.9.43", "185.34.194.93", "134.0.8.167", "185.34.194.74"]
-#----------------------------- srv01, srv02, srv03, srv04, srv05 = lista_servers
-
-#--------------------------------------------------------- #PARA PRUEBAS RAPIDAS
-#-------------------------------------------- lista_servers = ["185.34.192.190"]
-#--------------------------------------------------------- srv01 = lista_servers
-
-
-#---------------------------------------------- PARA HACER HEADLESS EL NAVEGADOR
-from selenium.webdriver.firefox.options import Options
-options = Options()
-options.headless = True
-
-#COMENZAR EL ARCHIVO
-file = open("./codigo.html", "w", encoding="utf-8")
-file.write(f"<html><body><h1>Listado de IP localizadas en blacklist (blacklistmaster.com) - {today}</h1><p>He sido generado con python</p>")
-
-
-#PARA COMPROBAR CADA ELEMENTO DE LA LISTA
-for srv in dct_servers:
-    #PARA HACER VISIBLE EL NAVEGADOR
-    #--------------------------------------------- browser = webdriver.Firefox()
-    #PARA HACER INVISIBLE EL NAVEGADOR
-    browser = webdriver.Firefox(options=options)
+#FUNCION PARA MODIFICAR EL LISTADO DE IP
+def modificar():
+    with open("./listado.json", "r") as read_file:
+        dct_servers = json.load(read_file)
     
-    browser.get("https://www.blacklistmaster.com/check")
-    search_form = browser.find_element_by_name('ip')
-    search_form.send_keys(dct_servers[srv])
-    boton = browser.find_element_by_name('Submit')
-    boton.click()
+    if any(dct_servers):
+        print('IP ya incluidas:')
+        for x in dct_servers:
+            sleep(1)
+            print(f'{x}: {dct_servers[x]}')
+    else:
+        print('No hay IP')
     
-    # HACER LA ESPERA DE 15 SEGUNDOS FUNCIONA PARA ESTA WEB https://www.blacklistmaster.com/check
-    sleep(15)
+    incluir_borrar = input('Quieres incluir o borrar una IP? (incluir/borrar): ')
+    if incluir_borrar == "incluir":
+        incluir_ip(dct_servers)
+    elif incluir_borrar == "borrar":
+        borrar_ip(dct_servers)
+    else:
+        print("No se hace nada")
+
+#FUNCION PARA INCLUIR UNA IP
+def incluir_ip(lista_ip):
+    sleep(1)
+    resultado = True
     
-    page = BeautifulSoup(browser.page_source,"html.parser")
-    browser.close()
+    while resultado:
+        nombre = input("Nombre del servidor a incluir: ")
+        ip = input("IP: ")
+        resultado = comprobar_ip(ip)
+
+    lista_ip[nombre] = ip
+    with open("./listado.json", "w") as write_file:
+        write_file.write(json.dumps(lista_ip))
+    print("Incluido")
+
+#FUNCION PARA BORRAR IP
+def borrar_ip(lista_ip):
+    sleep(1)    
+    comprobacion = True
     
-    #------------------------------ AQUI LOCALIZAMOS LA TABLA COMPLETA DE RESULTADOS
+    while comprobacion:
+        nombre = input('Nombre del servidor a borrar:')
+        comprobacion = revisar_srv(lista_ip,nombre)
     
-    links = page.select('.myip800 a')
-    print(srv,' - IP: ', dct_servers[srv])
+    with open("./listado.json", "w") as write_file:
+        write_file.write(json.dumps(lista_ip))
+    print("Borrado")
+
+#FUNCION PARA COMPROBAR QUE LA IP ES CORRECTA
+def comprobar_ip (ip):
+    numeros_ip = ip.split('.')
+    if len(numeros_ip) ==4:
+        for x in numeros_ip:
+            try:
+                int(x)
+                if (int(x)<0 or int(x)>255):
+                    print('ERROR EN LA IP: numero fuera de rango')
+                    return True
+            except:
+                print('ERROR EN LA IP: no es un numero')
+                return True
+    else:
+        print('ERROR EN LA IP: no tiene 4 campos')
+
+#FUNCION PARA COMPROBAR EL SERVIDOR PARA BORRAR
+def revisar_srv(ip,nombre):
+    try:
+        del ip[nombre]
+    except:
+        print('No se encuentra ese nombre de servidor')
+        return True
+
+
+#FUNCION PARA EJECUTAR EL ESCANEO
+def escanear():
+    with open("./listado.json", "r") as read_file:
+        dct_servers = json.load(read_file)
+    print('Estas monitorizando estas IP:')
+    for x in dct_servers:
+        sleep(1)
+        print(f'{x}: {dct_servers[x]}')
     
-    if links:
-        for link in links:
+    #FECHA
+    today = datetime.date.today()
+    
+    sleep(1)
+    print("Iniciando scrapeo")
+    
+    #PARA HACER HEADLESS EL NAVEGADOR
+    from selenium.webdriver.firefox.options import Options
+    options = Options()
+    options.headless = True
+    
+    #COMENZAR EL ARCHIVO
+    file = open("./codigo.html", "w", encoding="utf-8")
+    file.write(f"<html><body><h1>Listado de IP localizadas en blacklist (blacklistmaster.com) - {today}</h1><p>He sido generado con python</p>")
+    
+    
+    #PARA COMPROBAR CADA ELEMENTO DE LA LISTA
+    for srv in dct_servers:
+        #PARA HACER VISIBLE EL NAVEGADOR
+        #--------------------------------------------- browser = webdriver.Firefox()
+        #PARA HACER INVISIBLE EL NAVEGADOR
+        browser = webdriver.Firefox(options=options)
+        
+        browser.get("https://www.blacklistmaster.com/check")
+        search_form = browser.find_element_by_name('ip')
+        search_form.send_keys(dct_servers[srv])
+        boton = browser.find_element_by_name('Submit')
+        boton.click()
+        
+        # HACER LA ESPERA DE 15 SEGUNDOS FUNCIONA PARA ESTA WEB https://www.blacklistmaster.com/check
+        sleep(15)
+        
+        page = BeautifulSoup(browser.page_source,"html.parser")
+        browser.close()
+        
+        #------------------------------ AQUI LOCALIZAMOS LA TABLA COMPLETA DE RESULTADOS
+        
+        links = page.select('.myip800 a')
+        print(srv,' - IP: ', dct_servers[srv])
+        
+        if links:
+            for link in links:
+                file.write("<h3>")
+                file.write(dct_servers[srv])
+                file.write(": encontrado en ")
+                file.write(str(link))
+                file.write("</h3>")
+                print(f'{dct_servers[srv]}: encontrado en {link}')
+        else:
             file.write("<h3>")
             file.write(dct_servers[srv])
-            file.write(": encontrado en ")
-            file.write(str(link))
+            file.write(": Limpio")
             file.write("</h3>")
-            print(f'{dct_servers[srv]}: encontrado en {link}')
+            print(f'{dct_servers[srv]}: limpio')
+    
+        print(f"{srv} terminado")
+    
+    file.write("</html></body>")
+    file.close
+    
+    #----------------------- file = open("./codigo.html", "r", encoding="utf-8")
+    #--------------------------------------------------- resultado = file.read()
+    #---------------------------------------------------------------- file.close
+    
+    print('Fin del proceso')
+
+
+#RAMA PRINCIPAL DEL PROGRAMA
+
+programa = True
+
+print('Hola!')
+while programa:
+    iniciar_incluir = input("Quieres modificar el listado o ejecutar el escaneo? (modificar/ejecutar): ")
+    
+    if iniciar_incluir == "modificar":
+        print('Vamos a modificar el listado de IP')
+        sleep(1)
+        modificar()
+    elif iniciar_incluir == "ejecutar":
+        print('Ejecutando el escaneo')
+        sleep(1)
+        escanear()
+    elif iniciar_incluir == "salir":
+        print('Saliendo')
+        programa = False
     else:
-        file.write("<h3>")
-        file.write(dct_servers[srv])
-        file.write(": Limpio")
-        file.write("</h3>")
-        print(f'{dct_servers[srv]}: limpio')
-
-    print(f"{srv} terminado")
-
-file.write("</html></body>")
-file.close
-
-file = open("./codigo.html", "r", encoding="utf-8")
-resultado = file.read()
-file.close
-
-print('Fin de la busqueda')
+        print('No he entendido que quieres hacer')
 
 quit()
